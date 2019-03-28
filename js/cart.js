d5d036a6fab47b6b7c60ae936257dd24ed7a3dca
Alexander <maniacallion@gmail.com>
2019-03-28 15:50:10 -0500

    clearCart function ready to kill cart contents (line 23-26)

--- js/cart.js
+++ js/cart.js
@@ -20,7 +20,10 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  var cartClear = documnet.getElementsByTagName('td');
+  cartclear.remove();
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
